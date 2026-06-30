868ff0b95f13720ca386b3be27a9dc2ba78e5c6b
darkmasule <lastexcaliber@gmail.com>
2026-06-30 14:53:27 +0800

    new

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // line must always end in ;
+// use camel notation in js, lowercase first word and all uppercase after that firstName, keyColor, lowerUpperCase
 // variable always begin in let
 let name = 'Elite';
 // in js single colon '' is prefered
@@ -6,4 +7,8 @@ let name = 'Elite';
 // variables can't start with a number, 1name
 // can't contain with a space or a hyfen (-)
 
-//constant
+//constant = const
+// constant can't be change unlike variables. Use const if you don't want a variable to change
+const interestRate = 1
+
+console.log (name);
