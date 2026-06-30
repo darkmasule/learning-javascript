6376b3b3d7add929923dd24e09c56c5e0eb83ee0
darkmasule <lastexcaliber@gmail.com>
2026-06-30 14:38:43 +0800

    new

---
+++ script.js
@@ -0,0 +1,9 @@
+// line must always end in ;
+// variable always begin in let
+let name = 'Elite';
+// in js single colon '' is prefered
+// variables are case sensitive. firstName and FirstName are different variables because of the upper and lower case f
+// variables can't start with a number, 1name
+// can't contain with a space or a hyfen (-)
+
+//constant
