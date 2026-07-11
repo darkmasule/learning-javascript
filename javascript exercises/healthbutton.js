4678fd6d0f5ff345fd4a0d1d3b606a4eb38571e5
darkmasule <lastexcaliber@gmail.com>
2026-07-11 15:02:26 +0800

    sa

---
+++ javascript exercises/healthbutton.js
@@ -0,0 +1,53 @@
+//health
+let userHealth = 100;
+
+if (userHealth > 100) {
+    userHealth = 100
+} else if (userHealth < 0) {
+    alert('You died');
+}
+
+const plus10Button = document.getElementById("health10");
+
+plus10Button.addEventListener("click", () => {
+    userHealth = userHealth + 10
+    console.log(userHealth);
+})
+
+const plus25Button = document.getElementById("health25");
+
+plus25Button.addEventListener("click", () => {
+    userHealth = userHealth + 25
+    console.log(userHealth);
+})
+
+const plus50Button = document.getElementById("health50");
+
+plus50Button.addEventListener("click", () => {
+    userHealth = userHealth + 50
+    console.log(userHealth);
+})
+
+const minus10Button = document.getElementById("minus10");
+
+minus10Button.addEventListener("click", () => {
+    userHealth = userHealth - 10
+    console.log(userHealth);
+})
+
+const minus25Button = document.getElementById("minus25");
+
+minus25Button.addEventListener("click", () => {
+    userHealth = userHealth - 25
+    console.log(userHealth);
+})
+
+const minus50Button = document.getElementById("minus50");
+
+minus50Button.addEventListener("click", () => {
+    userHealth = userHealth - 50
+    console.log(userHealth);
+})
+
+
+//health
